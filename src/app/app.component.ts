37a3fb6b59b51a1d009fad4bffb0963328e6a8e6
kboul <kos.voulgaridis@gmail.com>
2018-10-06 13:25:28 +0300

    Initialize a basic map instance with marker, circle, polygon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { 
+	latLng, 
+	Map, 
+	tileLayer, 
+	circle, 
+	polygon, 
+	marker,
+	icon
+} from 'leaflet';
+
+import * as L from 'leaflet';
+
+import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon.png';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-leaflet-routing';
+	options = {
+		layers: [
+			tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+				maxZoom: 18, attribution: '...' 
+			})
+		],
+		zoom: 9,
+		center: latLng(46.879966, -121.726909)
+	};
+
+	layers = [
+		circle([ 46.95, -122 ], { radius: 5000 }),
+		polygon([[ 46.8, -121.85 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+		marker([ 46.879966, -121.726909 ], {
+			icon: icon({
+				iconSize: [ 25, 41 ],
+				iconAnchor: [ 13, 41 ],
+				iconUrl: 'leaflet/marker-icon.png',
+				shadowUrl: 'leaflet/marker-shadow.png'
+			  })
+		})
+	];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		LeafletModule.forRoot()
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
