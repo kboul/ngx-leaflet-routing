274f0c38b16f6de36c0714c4bba0cb4b486faf96
kboul <kos.voulgaridis@gmail.com>
2019-06-01 17:26:54 +0300

    Fix indentation on app.ts & app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { 
-	latLng, 
-	tileLayer, 
-	Icon, icon, Marker
+import {
+    latLng,
+    tileLayer,
+    Icon, icon, Marker
 } from 'leaflet';
 
 import 'leaflet';
@@ -10,38 +10,39 @@ import 'leaflet-routing-machine';
 declare let L;
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-	options = {
-		layers: [
-			tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
-				attribution: '© OpenStreetMap contributors'
-			})
-		],
-		zoom: 1,
-		center: latLng(46.879966, -121.726909)
-	};
+    options = {
+        layers: [
+            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap contributors'
+            })
+        ],
+        zoom: 1,
+        center: latLng(46.879966, -121.726909)
+    };
 
-	// Override default Icons
-	private defaultIcon: Icon = icon({
-		iconUrl: 'assets/marker-icon.png',
-		shadowUrl: 'assets/marker-shadow.png'
-	});
-	
-	ngOnInit() {
-		Marker.prototype.options.icon = this.defaultIcon;
-	}
+    // Override default Icons
+    private defaultIcon: Icon = icon({
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+    });
 
-	onMapReady(map: L.Map) {
-		L.Routing.control({
-			waypoints: [
-			  	L.latLng(57.74, 11.94),
-			  	L.latLng(57.6792, 11.949)
-			],
-			routeWhileDragging: true
-		}).addTo(map);
-	}
+    ngOnInit() {
+        Marker.prototype.options.icon = this.defaultIcon;
+    }
+
+    onMapReady(map: L.Map) {
+        L.Routing.control({
+            waypoints: [
+                L.latLng(57.74, 11.94),
+                L.latLng(57.6792, 11.949)
+            ],
+            routeWhileDragging: true
+        }).addTo(map);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AppComponent } from './app.component';
 
 @NgModule({
-	declarations: [
-		AppComponent
-	],
-	imports: [
-		BrowserModule,
-		LeafletModule.forRoot()
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        LeafletModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
