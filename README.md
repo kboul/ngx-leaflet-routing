23bfc13fc7fac97a04f7865b437a1ad6dbc43ae8
kboul <kos.voulgaridis@gmail.com>
2020-02-26 20:35:02 +0200

    Upgrade to Angular 9, (dev) dependencies & make indentation 4 to all files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NgxLeafletRouting
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.3.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,40 @@
-import { Component } from '@angular/core';
-import {
-    latLng,
-    tileLayer,
-    Icon, icon, Marker
-} from 'leaflet';
+import { Component } from "@angular/core";
+import { latLng, tileLayer, Icon, icon, Marker } from "leaflet";
 
-import 'leaflet';
-import 'leaflet-routing-machine';
+import "leaflet";
+import "leaflet-routing-machine";
 declare let L;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
-
 export class AppComponent {
-    options = {
-        layers: [
-            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap contributors'
-            })
-        ],
-        zoom: 1,
-        center: latLng(46.879966, -121.726909)
-    };
+	options = {
+		layers: [
+			tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				attribution: "© OpenStreetMap contributors"
+			})
+		],
+		zoom: 1,
+		center: latLng(46.879966, -121.726909)
+	};
 
-    // Override default Icons
-    private defaultIcon: Icon = icon({
-        iconUrl: 'assets/marker-icon.png',
-        shadowUrl: 'assets/marker-shadow.png'
-    });
+	// Override default Icons
+	private defaultIcon: Icon = icon({
+		iconUrl: "assets/marker-icon.png",
+		shadowUrl: "assets/marker-shadow.png"
+	});
 
-    ngOnInit() {
-        Marker.prototype.options.icon = this.defaultIcon;
-    }
+	ngOnInit() {
+		Marker.prototype.options.icon = this.defaultIcon;
+	}
 
-    onMapReady(map: L.Map) {
-        L.Routing.control({
-            waypoints: [
-                L.latLng(57.74, 11.94),
-                L.latLng(57.6792, 11.949)
-            ],
-            routeWhileDragging: true
-        }).addTo(map);
-    }
+	onMapReady(map: L.Map) {
+		L.Routing.control({
+			waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+			routeWhileDragging: true
+		}).addTo(map);
+	}
 }
