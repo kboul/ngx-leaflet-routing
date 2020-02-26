6c31b869ae1688ea5e380ffef29a03da48c2f05f
kboul <kos.voulgaridis@gmail.com>
2020-02-26 20:35:02 +0200

    Upgrade to Angular 9, (dev) dependencies & make indentation 4 to all files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NgxLeafletRouting
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        LeafletModule.forRoot()
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, LeafletModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
